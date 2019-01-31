02ca0be7f66b653f1bbe2c8a7b16d5272132795c
Viktoria Shchypalkina <37249354+helavika@users.noreply.github.com>
2019-01-31 22:43:54 +0200

    Update index.js

--- test3/js/index.js
+++ test3/js/index.js
@@ -2,16 +2,18 @@
 
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
 
-console.log(alphabet.slice(0, 12));
+var string = alphabet.slice(0, 12);
 
-console.log(alphabet.slice(12, 23));
+var string1 = alphabet.slice(12, 23);
 
-console.log(alphabet.slice(23));
+var string2 = alphabet.slice(23);
 
-console.log(alphabet[Math.floor(Math.random()*alphabet.length)]);
+console.log(string, string1, string2);
+
+console.log(alphabet[Math.round(Math.random()*alphabet.length-1)+1]);
 
 function generateRandomInteger(min, max) {
-  return Math.floor(min + Math.random()*(max+1 - min))
+  return Math.round(min + Math.random()*(max - min))
 }
 
-console.log(alphabet[Math.floor(Math.random()*12) + 1], alphabet[generateRandomInteger(12, 23)], alphabet[generateRandomInteger(23, alphabet.length-1)]);
+console.log(alphabet[Math.round(Math.random()*11) + 1], alphabet[generateRandomInteger(12, 22)], alphabet[generateRandomInteger(23, alphabet.length-1)]);
